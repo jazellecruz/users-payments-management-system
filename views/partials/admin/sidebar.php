bd6cb12413eb26b4e9b509140b37ea10e402260f
Jazz Cruz <jazellecruz22@gmail.com>
2025-10-13 00:15:06 +0800

    add dashboard components for admin and business rep

---
+++ views/partials/admin/sidebar.php
@@ -0,0 +1,35 @@
+<div class="vh-100 d-none d-md-block bg-brand-primary text-white" style="width: 280px;">
+    <div class="d-flex flex-column justify-content-between h-100">
+        <div class="pt-3">
+            <div class="d-flex gap-2 px-3">
+                    <i class="bi bi-grid-fill ml-2"></i>
+                    <p class="mb-0">Dashboard
+                </p>
+            </div>
+            <div class="border-bottom border-secondary my-3"></div>
+            <div class="flex-column d-flex gap-2 px-3">
+                <p class="mb-0">
+                    <i class="bi bi-currency-dollar"></i>
+                    Transactions
+                </p>
+                <div>
+                    <ul class="d-flex flex-column gap-2 mb-0" style="list-style-type: none; padding: left 50px;">
+                        <li><a class="nav-link fw-light text-light-gray" href="" style="">Payments</a></li>
+                        <li><a class="nav-link fw-light text-light-gray" href="" style="">Payouts</a></li>
+                        <li><a class="nav-link fw-light text-light-gray" href="" style="">Refunds</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="border-bottom border-secondary my-3"></div>
+        </div>
+        <div>
+            <div class="border-bottom border-secondary my-3"></div>
+            <div>
+                <ul class="pl-3 d-flex flex-column gap-3" style="list-style-type: none;">
+                    <li><a class="nav-link d-flex gap-2" href="" style=""><i class="bi bi-person-circle mr-2"></i><span class="text-light-gray">My Profile</span></a></li>
+                    <li><a class="nav-link d-flex gap-2" href="" style=""><i class="bi bi-power mr-2"></i><span class="text-light-gray">Log Out</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/partials/business_rep/sidebar.php
@@ -0,0 +1,36 @@
+<div class="vh-100 d-none d-md-block bg-brand-primary text-white" style="width: 280px;">
+    <div class="d-flex flex-column justify-content-between h-100">
+        <div class="pt-3">
+            <div class="d-flex gap-2 px-3">
+                    <i class="bi bi-grid-fill ml-2"></i>
+                    <p class="mb-0">Dashboard
+                </p>
+            </div>
+            <div class="border-bottom border-secondary my-3"></div>
+            <div class="px-3">
+                <div class="d-flex flex-column gap-4" style="list-style-type: none; padding: left 50px;">
+                    <div class="d-flex flex-row align-items-center gap-2">
+                        <i class="bi bi-file-earmark-text-fill ml-2"></i>
+                        <a class="nav-link fw-light text-light-gray" href="" style="">My Applications</a>
+                    </div>
+                    <div class="d-flex flex-row align-items-center gap-2">
+                        <i class="bi  bi-building-fill ml-2"></i>
+                        <a class="nav-link fw-light text-light-gray" href="" style="">My Businesses</a>
+                    </div>
+                    <div class="d-flex flex-row align-items-center gap-2">
+                        <i class="bi bi-person-fill  ml-2"></i>
+                        <a class="nav-link fw-light text-light-gray" href="" style="">My Profile</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="border-bottom border-secondary my-3"></div>
+            <div>
+                <ul class="pl-3 d-flex flex-column gap-2" style="list-style-type: none;">
+                    <li><a class="nav-link d-flex gap-2" href="" style=""><i class="bi bi-power mr-2"></i><span class="text-light-gray">Log Out</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
